6b9c877aaea2d55a2be7271ba31e301992a4c45f
Oleg11_508 <oleg_o97@mail.ru>
2019-11-10 22:26:20 +0300

    v0.1.0.3 changed errorHandling

--- src/test/java/ru/osipov/deploy/web/GenreControllerTest.java
+++ src/test/java/ru/osipov/deploy/web/GenreControllerTest.java
@@ -45,14 +45,6 @@ public class GenreControllerTest {
     GenreService gs;
 
     @Test
-    void testP(){
-        List<Integer> l = new LinkedList<>();
-        l.add(1);l.add(2);l.add(3);l.add(4);l.add(5);
-        l = l.subList(1,4);//1,2,3
-        assert l.size() == 3;
-    }
-
-    @Test
     void testAll() throws Exception {
         logger.info("testAll");
         final GenreInfo g1 = new GenreInfo(1L,PARAMS1[0], PARAMS1[1]);
